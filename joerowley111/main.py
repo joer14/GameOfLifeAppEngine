7b5fb3bab9fbe91b58ce8d7c58c360731c23de42
Joe Rowley <joer14@gmail.com>
2013-02-20 20:57:41 -0800

    It's cleaning time!

--- joerowley111/main.py
+++ joerowley111/main.py
@@ -14,12 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import csv
+import os
+import re
+import random
+from google.appengine.api import users
+
+from collections import defaultdict
+
 import webapp2
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Hello World')
+
+
+class Dashboard(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/dashboard.html')
+        self.response.write(template.render(path, {}))
+
+class Homepage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/home.html')
+        self.response.write(template.render(path, {}))
+
+class Aboutpage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/about.html')
+        self.response.write(template.render(path, {}))
+
+class Contactpage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/contact.html')
+        self.response.write(template.render(path, {}))
+
+
+
+class NotFoundPageHandler(webapp2.RequestHandler):
+    def get(self):
+        self.error(404)
+        dognum = random.randint(0, 4)
+        #self.response.out.write('Your 404 error html page')
+        output = {
+            'dognum': dognum,
+            }
+        path = os.path.join(os.path.dirname(__file__), 'templates/404.html')
+        self.response.write(template.render(path, output))
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', Homepage),
+                                  ('/dashboard', Dashboard),
+                                  ('/about', Aboutpage),
+                                  ('/contact', Contactpage),
+                                  ('/.*', NotFoundPageHandler),
+                                  ], debug=True)
